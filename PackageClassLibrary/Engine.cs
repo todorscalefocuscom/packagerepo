7b84f554fc6899c4665ef3a21bc74194cd6bb5be
todorscalefocuscom <100688603+todorscalefocuscom@users.noreply.github.com>
2022-03-04 09:31:28 +0200

    Create Engine.cs

---
+++ PackageClassLibrary/Engine.cs
@@ -0,0 +1,17 @@
+namespace PackageClassLibrary
+{
+    public class Engine : IEngine
+    {
+        public void Drive()
+        {
+        }
+
+        public void Reverse()
+        {
+        }
+
+        public void Stop()
+        {
+        }
+    }
+}
